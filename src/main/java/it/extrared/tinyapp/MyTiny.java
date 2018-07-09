b8b2753ca9fcb38f8172dc92cbb9981860c3088c
davide.costanza <davide.costanza@extrasys.it>
2018-07-09 12:20:04 +0200

    Aggiunto codice Java e unit test

---
+++ src/main/java/it/extrared/tinyapp/MyTiny.java
@@ -0,0 +1,6 @@
+public class MyTiny {
+
+    public String concatenate(String one, String two){
+        return one + two;
+    }
+}
---
+++ src/test/java/it/extrared/tinyapp/MyTinyTest.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class MyTinyTest {
+
+    @Test
+    public void testConcatenate() {
+        MyTiny myTiny = new MyTiny();
+
+        String result = myTiny.concatenate("one", "two");
+
+        assertEquals("onetwo", result);
+    }
+}
+
